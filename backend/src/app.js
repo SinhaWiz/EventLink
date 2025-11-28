228cbe33428768e84e2c7b237a31a80655e5ace1
SinhaWiz <sinhartx2070@gmail.com>
2025-11-28 18:14:57 +0600

    routers integrated in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,9 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 
+// Route files
+const auth = require('./routes/authRoutes');
+
 const app = express();
 
 // Middleware
@@ -10,6 +13,9 @@ app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
+
+// Mount routers
+app.use('/api/auth', auth);
 
 // Basic Route
 app.get('/', (req, res) => {
