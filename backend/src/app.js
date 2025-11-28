df873a3e06bbc324cb4224d2394e4988318678ff
SinhaWiz <sinhartx2070@gmail.com>
2025-11-28 18:07:47 +0600

    first route created

---
+++ backend/src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+
+// Basic Route
+app.get('/', (req, res) => {
+  res.send('EventLink API is running...');
+});
+
+module.exports = app;
